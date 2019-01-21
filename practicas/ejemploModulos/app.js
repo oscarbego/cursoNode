5c4c3285769de846b70ba6346daec6779fb6cbac
oscarbego <oscar.bego@gmail.com>
2019-01-21 09:53:15 -0700

    agregando ejemplo de módulos

---
+++ practicas/ejemploModulos/app.js
@@ -0,0 +1,5 @@
+// Utilizamos la función require() El nombre del archivo se ingresa sin la extensión (opcional) .js 
+var square = require('./square');
+
+// invocamos el metodo area()
+console.log('El área de un cuadrado con lado de 4 es ' + square.area(4));
